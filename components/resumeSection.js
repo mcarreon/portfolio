5b016da37adab53c93c714e86871bb45bbe55f73
mcarreon <carryon2095@gmail.com>
2019-08-31 02:26:02 -0700

    added resume section

---
+++ components/resumeSection.js
@@ -0,0 +1,45 @@
+const ResumeSection = ({ data, type }) => {
+  if (type === "experience") {
+    const { name, title, dates, length, location, description, link } = data;
+
+    return (
+      <div className="mt-4">
+        {link ? <a href={link}>{name}</a> : <h3>{name}</h3>}
+        <h4>{title}</h4>
+        <p className="my-1">{`${dates[0]} - ${dates[1]} • ${length}`}</p>
+        <ul className="pl-4">
+          {description.map(desc => (
+            <li className="pb-3">{desc}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else if (type === "education") {
+    const { name, title, subtitle, dates, link } = data;
+
+    return (
+      <div className="mt-4">
+        {link ? <a href={link}>{name}</a> : <h3>{name}</h3>}
+        <h4>{title}</h4>
+        <p className="my-1">{name === "UCLA" ? `${subtitle}` : ""}</p>
+        <p className="my-1">
+          {name === "UCLA" ? `${dates[0]}` : `${dates[0]} - ${dates[1]}`}
+        </p>
+      </div>
+    );
+  } else if (type === "skills" || type === "tools") {
+    const { name, skills } = data;
+    const parseSkills = skills.join(', ');
+
+    return (
+      <div className="mt-4 skills">
+        <h3>{name}</h3>
+        <p>{parseSkills}</p>
+      </div>
+    );
+  }
+
+  return <div></div>;
+};
+
+export default ResumeSection;
--- pages/resume.js
+++ pages/resume.js
@@ -1,7 +1,47 @@
-const Resume = () => (  
-  <div>
-    
-  </div>
-);
+import resume from '../static/resume.json';
+import ResumeSection from '../components/resumeSection';
+
+const Resume = () => {
+  
+  return (
+    <div className="resume mt-6 container">
+      <h2 className="mb-3">Resume</h2>
+      <div className="resume--body border-y">
+        <div className="border-bottom">
+          <h3>{resume.experience.type}</h3>
+          <div>
+            {resume.experience.data.map(data => (
+              <ResumeSection data={data} type="experience"/>  
+            ))}
+          </div>
+        </div>
+        <div className="border-bottom pb-4">
+          <h3>{resume.education.type}</h3>
+          <div>
+            {resume.education.data.map(data => (
+              <ResumeSection data={data} type="education"/>  
+            ))}
+          </div>
+        </div>
+        <div className="border-bottom pb-4">
+          <h3>{resume.skills.type}</h3>
+          <div>
+            {resume.skills.data.map(data => (
+              <ResumeSection data={data} type="skills"/>  
+            ))}
+          </div>
+        </div>
+        <div className="pb-4">
+          <h3>{resume.tools.type}</h3>
+          <div>
+            {resume.tools.data.map(data => (
+              <ResumeSection data={data} type="tools"/>  
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Resume;
